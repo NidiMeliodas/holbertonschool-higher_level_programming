a5ce11be6a5069598d80602dd8518fcadf12278a
NidiMeliodas <nidigashan@gmail.com>
2024-01-22 17:10:11 +0100

    task 6

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66], str[107:111], str[0:6])
+# ff
