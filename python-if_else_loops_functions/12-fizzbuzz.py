ea2dee3d40d10138ccfbf03c20538af713581108
NidiMeliodas <nidigashan@gmail.com>
2024-01-26 20:44:51 +0100

    fix 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    fizz = i % 3
-    buzz = i % 5
+def fizzbuzz():
+    for i in range(1, 101):
+        fizz = i % 3
+        buzz = i % 5
+        if fizz == 0 and buzz == 0:
+            print("FizzBuzz", end=' ')
+            continue
+        if fizz == 0:
+            print("Fizz", end=' ')
+            continue
+        if buzz == 0:
+            print("Buzz", end=' ')
+            continue
 
-    if fizz == 0 and buzz == 0:
-        print("FizzBuzz", end=' ')
-        continue
-    if fizz == 0:
-        print("Fizz", end=' ')
-        continue
-    if buzz == 0:
-        print("Buzz", end=' ')
-        continue
-    else:
         print(i, end=' ')
+
