cd8cf30202f22683985cc6ca782dcd8ed20e8e89
NidiMeliodas <nidigashan@gmail.com>
2024-01-26 16:33:17 +0100

    fixing 2 print alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char_code in range(ord('a'), ord('z') + 1):
-    char = chr(char_code)
-    print(char, end='')
+
+for char in range(97, 123):
+    print("{}".format(chr(char)), end="")
