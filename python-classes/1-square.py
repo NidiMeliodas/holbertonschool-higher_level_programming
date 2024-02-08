77be4a3b0ab6e48d3ba756ae235b78626a1e498a
NidiMeliodas <nidigashan@gmail.com>
2024-02-08 03:47:33 +0100

    fix task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,5 +3,7 @@
 
 
 class Square:
+    '''Class : Square with a private instance attribut'''
     def __init__(self, size):
+        '''initalize square using pirvate attribute size as args'''
         self._size = size
