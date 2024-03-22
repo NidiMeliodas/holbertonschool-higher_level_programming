dfb0aa6457d7faf7bd4ab71aedebda4e16210290
NidiMeliodas <nidigashan@gmail.com>
2024-03-22 17:34:44 +0100

    fix 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
     cursor = database.cursor()
     cursor.execute("""SELECT cities.name FROM cities
                    JOIN states ON cities.state_id = states.id
+                   WHERE states.name = %s
                    ORDER BY cities.id""", (sys.argv[4],))
 
     results = cursor.fetchall()
