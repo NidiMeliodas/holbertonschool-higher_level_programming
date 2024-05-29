20d2dd83b88251315cfd3029ca9937581013f660
NidiMeliodas <nidigashan@gmail.com>
2024-05-29 22:37:14 +0200

    Task 1 - javascript-dom_manipulation

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+document.getElementById("red_header").addEventListener("click", changeColour);
+
+function changeColour() {
+  let redHeader = document.getElementById("red_header");
+  redHeader.style.color = "#FF0000";
+}
